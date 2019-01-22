dcad483c25596909ce6bc513445a566a095196ad
Unknown <billhuang0916@gmail.com>
2019-01-21 22:38:24 -0600

    Finished Automatic Gear Shifting

--- FRC-Robot-2019/src/main/java/frc/robot/commands/AutomaticGearShiftCommand.java
+++ FRC-Robot-2019/src/main/java/frc/robot/commands/AutomaticGearShiftCommand.java
@@ -2,40 +2,74 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
+import frc.robot.subsystems.DriveTrainSubsystem;
+import frc.robot.subsystems.GearShifterSubsystem;
+import frc.robot.subsystems.GearShifterSubsystem.Gear;
 
 /**
- * Automatically shifts the 
+ * Automatically shifts the gears up and down
  */
 public class AutomaticGearShiftCommand extends Command {
+  private final double MAXSPEED_IN_INCHES_PER_SECOND = 15 * 12; //TODO: Find real values for these constants
   private final double UPSHIFT_SPEED_LOW = 0.4;
   private final double UPSHIFT_SPEED_HIGH = 0.4;
   private final double UPSHIFT_THROTTLE_LOW = 0.3;
   private final double UPSHIFT_THROTTLE_HIGH = 0.6;
-  private final double DOWNSSHIFT_SPEED_LOW = 0.1;
-  private final double DOWNSSHIFT_SPEED_HIGH = 0.1;
+  private final double DOWNSHIFT_SPEED_LOW = 0.1;
+  private final double DOWNSHIFT_SPEED_HIGH = 0.1;
   private final double DOWNSHIFT_THROTTLE_LOW = 0.3;
   private final double DOWNSHIFT_THROTTLE_HIGH = 0.6;
   private final double DEADZONE = 0.02; 
   private final double MAX_OUTPUT = 1.0;
   private final double RIGHT_SIDE_INVERT_MULTIPLIER = -1.0;
-  private double leftThrottle;
-  private double rightThrottle;
+
+  private GearShifterSubsystem gearShifter;
+  private DriveTrainSubsystem driveTrain;
+
 
   public AutomaticGearShiftCommand() {
     // Use requires() here to declare subsystem dependencies
     requires(Robot.gearShifter);
+    gearShifter = Robot.gearShifter;
+    driveTrain = Robot.driveTrain;
   }
 
-  // Called repeatedly when this Command is scheduled to run
+  /**
+   * Shifts the gear once the current speed passes a threshold
+   */
   @Override
   protected void execute() {
+    //TODO: Use Navx to prevent gear shifting while turning
+    Gear curGear = gearShifter.getCurGear();
+    //The line below may break because the encoders belong to the DriveTrain subsystem
+    double curSpeed = (Math.abs(driveTrain.getLeftEncoder().getRate()) + Math.abs(driveTrain.getLeftEncoder().getRate())) / 2.0;
+    double shiftSpeed = getShiftSpeed(curGear, getThrottle(true));
     
+    if(curGear == Gear.LOW_GEAR) {
+      if(curSpeed > shiftSpeed) {
+        gearShifter.shiftUp();
+      }
+    } else {
+      //Comment out to test upshifting before downshifting
+      // if(curSpeed < shiftSpeed) {
+      //   gearShifter.shiftDown();
+      // }
+    }
+  }
+
+  @Override
+  protected boolean isFinished() {
+    return false;
   }
 
   /**
    * Taken from DifferentialDrive arcadeDrive method
+   * Gets the minimum throttle between the two sides of the robot
+   * 
+   * @param squareInputs Whether you are using squared inputs
+   * @return The minimum throttle
    */
-  private void setThrottles(boolean squareInputs) {
+  private double getThrottle(boolean squareInputs) {
     double xSpeed = limit(Robot.m_oi.getController0().getLeftJoystickY());
     xSpeed = applyDeadband(xSpeed, DEADZONE);
 
@@ -74,8 +108,9 @@ public class AutomaticGearShiftCommand extends Command {
       }
     }
 
-    leftThrottle = limit(leftMotorOutput) * MAX_OUTPUT;
-    rightThrottle = limit(rightMotorOutput) * MAX_OUTPUT * RIGHT_SIDE_INVERT_MULTIPLIER;
+    double leftThrottle = Math.abs(limit(leftMotorOutput) * MAX_OUTPUT);
+    double rightThrottle = Math.abs(limit(rightMotorOutput) * MAX_OUTPUT * RIGHT_SIDE_INVERT_MULTIPLIER);
+    return Math.min(leftThrottle, rightThrottle);
   }
 
     /**
@@ -98,7 +133,7 @@ public class AutomaticGearShiftCommand extends Command {
    * @param value    value to clip
    * @param deadband range around zero
    */
-  protected double applyDeadband(double value, double deadband) {
+  private double applyDeadband(double value, double deadband) {
     if (Math.abs(value) > deadband) {
       if (value > 0.0) {
         return (value - deadband) / (1.0 - deadband);
@@ -110,9 +145,33 @@ public class AutomaticGearShiftCommand extends Command {
     }
   }
 
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  protected boolean isFinished() {
-    return false;
+  /**
+   * Calculates the shift speed threshold based on the current gear and throttle
+   * @param curGear The current gear
+   * @param curThrottle The current minimum throttle
+   * @return The shift speed threshold
+   */
+  private double getShiftSpeed(Gear curGear, double curThrottle) {
+    if(curGear == Gear.LOW_GEAR) {
+      if(curThrottle < UPSHIFT_THROTTLE_LOW) {
+        return MAXSPEED_IN_INCHES_PER_SECOND * UPSHIFT_SPEED_LOW;
+      } else if(curThrottle < UPSHIFT_SPEED_HIGH) {
+        double slope = (UPSHIFT_SPEED_HIGH - UPSHIFT_SPEED_LOW) / (UPSHIFT_THROTTLE_HIGH - UPSHIFT_THROTTLE_LOW);
+        double maxSpeedProportion = UPSHIFT_SPEED_LOW + slope * (curThrottle - UPSHIFT_THROTTLE_LOW);
+        return maxSpeedProportion * MAXSPEED_IN_INCHES_PER_SECOND;
+      } else {
+        return MAXSPEED_IN_INCHES_PER_SECOND * UPSHIFT_SPEED_HIGH;
+      }
+    } else {
+      if(curThrottle < DOWNSHIFT_THROTTLE_LOW) {
+        return MAXSPEED_IN_INCHES_PER_SECOND * DOWNSHIFT_SPEED_LOW;
+      } else if(curThrottle < DOWNSHIFT_SPEED_HIGH) {
+        double slope = (DOWNSHIFT_SPEED_HIGH - DOWNSHIFT_SPEED_LOW) / (DOWNSHIFT_THROTTLE_HIGH - DOWNSHIFT_THROTTLE_LOW);
+        double maxSpeedProportion = DOWNSHIFT_SPEED_LOW + slope * (curThrottle - DOWNSHIFT_THROTTLE_LOW);
+        return maxSpeedProportion * MAXSPEED_IN_INCHES_PER_SECOND;
+      } else {
+        return MAXSPEED_IN_INCHES_PER_SECOND * DOWNSHIFT_SPEED_HIGH;
+      }
+    }
   }
 }
--- FRC-Robot-2019/src/main/java/frc/robot/subsystems/GearShifterSubsystem.java
+++ FRC-Robot-2019/src/main/java/frc/robot/subsystems/GearShifterSubsystem.java
@@ -5,32 +5,40 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Sendable;
 import frc.robot.RobotMap;
+import frc.robot.commands.AutomaticGearShiftCommand;
 
 public class GearShifterSubsystem extends Subsystem {
     private DoubleSolenoid gearShiftSolenoid = new DoubleSolenoid(RobotMap.GEARSHIFT_SOLENOID_CAN_ID, RobotMap.GEARSHIFTUP_SOLENOID_ID, RobotMap.GEARSHIFTDOWN_SOLENOID_ID);
 
-    private enum Gear {
+    public static enum Gear {
         LOW_GEAR, HIGH_GEAR
     }
 
-    private Gear currentGear = Gear.LOW_GEAR;
+    private Gear curGear;
     
     public GearShifterSubsystem() {
         addChild("PCM", (Sendable) gearShiftSolenoid);
     }
 
     public void initDefaultCommand() {
-        
+        curGear = Gear.HIGH_GEAR;
+        shiftDown();
+        setDefaultCommand(new AutomaticGearShiftCommand());
     }
 
     public void shiftUp() {
-        if(currentGear == Gear.LOW_GEAR)
+        if(curGear != Gear.HIGH_GEAR)
             gearShiftSolenoid.set(DoubleSolenoid.Value.kForward);
+        curGear = Gear.HIGH_GEAR;
     }
 
     public void shiftDown() {
-        if(currentGear == Gear.HIGH_GEAR)
+        if(curGear != Gear.HIGH_GEAR)
             gearShiftSolenoid.set(DoubleSolenoid.Value.kReverse);
+        curGear = Gear.LOW_GEAR;
     }
 
+    public Gear getCurGear() {
+        return curGear;
+    }
 }
