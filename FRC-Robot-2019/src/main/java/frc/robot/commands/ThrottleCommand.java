53c4bd9a8fe252dae6709c0257e50806b48b4691
Unknown <billhuang0916@gmail.com>
2019-01-23 22:14:31 -0600

    Added ThrottleCommand

---
+++ FRC-Robot-2019/src/main/java/frc/robot/commands/ThrottleCommand.java
@@ -0,0 +1,63 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.subsystems.DriveTrainSubsystem;
+
+/**
+ * An example command. You can replace me with your own command.
+ */
+public class ThrottleCommand extends Command {
+  private final double CYCLE_TIME_IN_SECONDS = 0.020;
+  private DriveTrainSubsystem driveTrain;
+  private int cycleCount;
+  private int maxCycles;
+  private double stepValue;
+  private double startValue;
+
+  /**
+   * 
+   * @param rampTime Ramp up time in seconds
+   */
+  public ThrottleCommand(double rampTime, double startValue, double endValue) {
+    // Use requires() here to declare subsystem dependencies
+    requires(Robot.driveTrain);
+    driveTrain = Robot.driveTrain;
+    cycleCount = 0;
+    maxCycles = (int) Math.ceil(rampTime / CYCLE_TIME_IN_SECONDS);
+    this.startValue = startValue;
+    stepValue = (endValue - startValue) / maxCycles;
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    driveTrain.stop();
+  }
+
+  // Read Controller Input from two joysticks.
+  // Left joystick controls the left motors and the right joystick controls the
+  // right motors
+  // Temporary until we get the XboxController wrapper for joystick
+  @Override
+  protected void execute() {
+    double throttle = Robot.m_oi.getController0().getLeftJoystickY() * (startValue + stepValue * cycleCount);
+    cycleCount++;
+    Robot.driveTrain.ArcadeDrive(throttle, Robot.m_oi.getController0().getLeftJoystickX(), true);
+  }
+
+  @Override
+  protected boolean isFinished() {
+    return cycleCount >= maxCycles;
+  }
+
+  @Override
+  protected void end() {
+    driveTrain.stop();
+  }
+
+  @Override
+  protected void interrupted() {
+    return;
+  }
+}
