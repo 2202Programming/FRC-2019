f2b9f85a0c958bc33004069d438b2d4c30fa61d7
Unknown <michael.lin2032@gmail.com>
2019-01-26 11:43:17 -0600

    Added Method

--- FRC-Robot-2019/src/main/java/frc/robot/commands/FootMoverCommand.java
+++ FRC-Robot-2019/src/main/java/frc/robot/commands/FootMoverCommand.java
@@ -1,17 +1,39 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.Encoder;
+import frc.robot.Robot;
 
 public class FootMoverCommand extends Command
 {
-    
+    private Encoder moveEncoder = Robot.climber.getEncoder();//775 motor checker for distance
+
+    public FootMoverCommand()
+    {
+        // Use requires() here to declare subsystem dependencies
+        requires(Robot.climber);
+    }
 
     @Override
     protected boolean isFinished()
     {
-
+        //This needs to be changed depending on requirements
+        if(moveEncoder.getDistance() >= 5.0/*fake value*/)
+            return true;
+        return false;
     }
 
+
+    @Override
+    protected void initialize()
+    {
+        Robot.climber.stopFoot();
+    }
+    @Override
+    protected void execute()
+    {
+        Robot.climber.moveFoot();
+    }
     @Override
     protected void end()
     {
