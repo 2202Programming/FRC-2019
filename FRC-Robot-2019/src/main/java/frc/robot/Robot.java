0991dca9bd8b09c62f87e5a56d2616847a0a376b
Unknown <billhuang0916@gmail.com>
2019-01-26 15:36:16 -0600

    Prepare for Testing

--- FRC-Robot-2019/src/main/java/frc/robot/Robot.java
+++ FRC-Robot-2019/src/main/java/frc/robot/Robot.java
@@ -7,7 +7,12 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -25,9 +30,19 @@ import frc.robot.subsystems.GearShifterSubsystem;;
  */
 public class Robot extends TimedRobot {
   public static OI m_oi;
-  public static DriveTrainSubsystem driveTrain = new DriveTrainSubsystem();
-  public static GearShifterSubsystem gearShifter = new GearShifterSubsystem();
+  public static DriveTrainSubsystem driveTrain;
+  public static GearShifterSubsystem gearShifter;
 
+   /* Hardware */
+   TalonSRX _talon = new TalonSRX(11);
+   Joystick _joy = new Joystick(0);
+
+   /* String for output */
+   StringBuilder _sb = new StringBuilder();
+
+   /* Loop tracker for prints */
+   int _loops = 0;
+   
   Command m_autonomousCommand;
   SendableChooser<Command> m_chooser = new SendableChooser<>();
 
@@ -40,6 +55,30 @@ public class Robot extends TimedRobot {
     m_oi = new OI();
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
+    /* Factory Default all hardware to prevent unexpected behaviour */
+    _talon.configFactoryDefault();
+
+    /* Config sensor used for Primary PID [Velocity] */
+    _talon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, Constants.kPIDLoopIdx,
+            Constants.kTimeoutMs);
+
+    /**
+     * Phase sensor accordingly. Positive Sensor Reading should match Green
+     * (blinking) Leds on Talon
+     */
+    _talon.setSensorPhase(true);
+
+    /* Config the peak and nominal outputs */
+    _talon.configNominalOutputForward(0, Constants.kTimeoutMs);
+    _talon.configNominalOutputReverse(0, Constants.kTimeoutMs);
+    _talon.configPeakOutputForward(1, Constants.kTimeoutMs);
+    _talon.configPeakOutputReverse(-1, Constants.kTimeoutMs);
+
+    /* Config the Velocity closed loop gains in slot0 */
+    _talon.config_kF(Constants.kPIDLoopIdx, Constants.kGains_Velocit.kF, Constants.kTimeoutMs);
+    _talon.config_kP(Constants.kPIDLoopIdx, Constants.kGains_Velocit.kP, Constants.kTimeoutMs);
+    _talon.config_kI(Constants.kPIDLoopIdx, Constants.kGains_Velocit.kI, Constants.kTimeoutMs);
+    _talon.config_kD(Constants.kPIDLoopIdx, Constants.kGains_Velocit.kD, Constants.kTimeoutMs);
   }
 
   /**
@@ -127,6 +166,55 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     Scheduler.getInstance().run();
+    /* Get gamepad axis */
+    double leftYstick = -1 * _joy.getY();
+
+    /* Get Talon/Victor's current output percentage */
+    double motorOutput = _talon.getMotorOutputPercent();
+
+    /* Prepare line to print */
+    _sb.append("\tout:");
+    /* Cast to int to remove decimal places */
+    _sb.append((int) (motorOutput * 100));
+    _sb.append("%"); // Percent
+
+    _sb.append("\tspd:");
+    _sb.append(_talon.getSelectedSensorVelocity(Constants.kPIDLoopIdx));
+    _sb.append("u"); // Native units
+
+    /**
+     * When button 1 is held, start and run Velocity Closed loop. Velocity Closed
+     * Loop is controlled by joystick position x500 RPM, [-500, 500] RPM
+     */
+    if (_joy.getRawButton(1)) {
+        /* Velocity Closed Loop */
+
+        /**
+         * Convert 500 RPM to units / 100ms. 4096 Units/Rev * 500 RPM / 600 100ms/min in
+         * either direction: velocity setpoint is in units/100ms
+         */
+        double targetVelocity_UnitsPer100ms = leftYstick * 500.0 * 4096 / 600;
+        /* 500 RPM in either direction */
+        _talon.set(ControlMode.Velocity, targetVelocity_UnitsPer100ms);
+
+        /* Append more signals to print when in speed mode. */
+        _sb.append("\terr:");
+        _sb.append(_talon.getClosedLoopError(Constants.kPIDLoopIdx));
+        _sb.append("\ttrg:");
+        _sb.append(targetVelocity_UnitsPer100ms);
+    } else {
+        /* Percent Output */
+
+        _talon.set(ControlMode.PercentOutput, leftYstick);
+    }
+
+    /* Print built string every 10 loops */
+    if (++_loops >= 10) {
+        _loops = 0;
+        System.out.println(_sb.toString());
+    }
+    /* Reset built string */
+    _sb.setLength(0);
   }
 
   /**
@@ -137,18 +225,18 @@ public class Robot extends TimedRobot {
   }
 
   private void logSmartDashboardSensors() {
-    SmartDashboard.putNumber("Left Encoder Count", driveTrain.getLeftEncoderTalon().getSelectedSensorPosition());
-    SmartDashboard.putNumber("Left Encoder Rate", driveTrain.getLeftEncoderTalon().getSelectedSensorVelocity());
-    SmartDashboard.putNumber("Right Encoder Count", driveTrain.getRightEncoderTalon().getSelectedSensorPosition());
-    SmartDashboard.putNumber("Right Encoder Rate", driveTrain.getRightEncoderTalon().getSelectedSensorVelocity());
-    SmartDashboard.putString("Gear Shifter State", String.valueOf(gearShifter.getCurGear()));
-    SmartDashboard.putData(Scheduler.getInstance()); 
-    SmartDashboard.putData(driveTrain);
-    SmartDashboard.putData(gearShifter);
+    // SmartDashboard.putNumber("Left Encoder Count", driveTrain.getLeftEncoderTalon().getSelectedSensorPosition());
+    // SmartDashboard.putNumber("Left Encoder Rate", driveTrain.getLeftEncoderTalon().getSelectedSensorVelocity());
+    // SmartDashboard.putNumber("Right Encoder Count", driveTrain.getRightEncoderTalon().getSelectedSensorPosition());
+    // SmartDashboard.putNumber("Right Encoder Rate", driveTrain.getRightEncoderTalon().getSelectedSensorVelocity());
+    // SmartDashboard.putString("Gear Shifter State", String.valueOf(gearShifter.getCurGear()));
+    // SmartDashboard.putData(Scheduler.getInstance()); 
+    // SmartDashboard.putData(driveTrain);
+    // SmartDashboard.putData(gearShifter);
   }
 
   private void resetAllDashBoardSensors() {
-    driveTrain.getLeftEncoderTalon().setSelectedSensorPosition(0);
-    driveTrain.getRightEncoderTalon().setSelectedSensorPosition(0);
+    // driveTrain.getLeftEncoderTalon().setSelectedSensorPosition(0);
+    // driveTrain.getRightEncoderTalon().setSelectedSensorPosition(0);
   }
 }
--- FRC-Robot-2019/src/main/java/frc/robot/TalonRobot.java
+++ FRC-Robot-2019/src/main/java/frc/robot/TalonRobot.java
@@ -1,3 +1,5 @@
+package frc.robot;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
