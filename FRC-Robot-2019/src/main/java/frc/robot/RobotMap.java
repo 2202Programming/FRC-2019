c9bddd9f830fea6523230a0207ff2d4e2ee7c4e7
Xander Ge <xander.q.ge@gmail.com>
2019-02-01 17:59:19 -0600

    Added Arm Ports

--- FRC-Robot-2019/src/main/java/frc/robot/RobotMap.java
+++ FRC-Robot-2019/src/main/java/frc/robot/RobotMap.java
@@ -14,25 +14,32 @@ package frc.robot;
  * floating around.
  */
 public class RobotMap {
+  //Chassis
   public static final int FL_TALON_CAN_ID = 11;
   public static final int ML_TALON_CAN_ID = 12;
   public static final int BL_TALON_CAN_ID = 13;
   public static final int FR_TALON_CAN_ID = 14;
   public static final int MR_TALON_CAN_ID = 15;
   public static final int BR_TALON_CAN_ID = 16;
+
+  public static final int ENCODER_LEFT_PIN_1 = 0;
+  public static final int ENCODER_LEFT_PIN_2 = 1;
+  public static final int ENCODER_RIGHT_PIN_1 = 2;
+  public static final int ENCODER_RIGHT_PIN_2 = 3;
+
   public static final int GEARSHIFT_SOLENOID_CAN_ID = 2;
   public static final int GEARSHIFTUP_SOLENOID_ID = 1;
   public static final int GEARSHIFTDOWN_SOLENOID_ID = 3;
+  
+  //Arm
+  public static final int ARM_ROTATION_TALON_CAN_ID = 20;
+  public static final int ARM_MIN_ROTATION_SENSOR_PIN = 4;
+  public static final int ARM_ROTATION_ENCODER_PIN_0 = 5;
+  public static final int ARM_ROTATION_ENCODER_PIN_1 = 6;
 
-
-  public static final int CLIMBER_MOTOR_PIN = 4;
-  public static final int LEFT_INTAKE_MOTOR_PIN = 5;
-  public static final int RIGHT_INTAKE_MOTOR_PIN = 6;
-
-  public static final int INTAKE_PHOTOGATE_CHANNEL = 4;
-
-  public static final int ENCODER_LEFT_PIN_1 = 1;
-  public static final int ENCODER_LEFT_PIN_2 = 2;
-  public static final int ENCODER_RIGHT_PIN_1 = 3;
-  public static final int ENCODER_RIGHT_PIN_2 = 4;
+  public static final int ARM_EXTENSTION_TALON_CAN_ID = 21;
+  public static final int ARM_MIN_EXTENSION_SENSOR_PIN = 7;
+  public static final int ARM_EXTENSION_ENCODER_PIN_0 = 8;
+  public static final int ARM_EXTENSION_ENCODER_PIN_1 = 9;
+  
 }
