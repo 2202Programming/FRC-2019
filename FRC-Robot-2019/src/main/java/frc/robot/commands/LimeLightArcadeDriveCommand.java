150cf317ba55b4a5d900cc883dae49bcefd0811f
Unknown <billhuang0916@gmail.com>
2019-02-01 18:38:36 -0600

    Added Base Subsystem and Command

---
+++ FRC-Robot-2019/src/main/java/frc/robot/commands/LimeLightArcadeDriveCommand.java
@@ -0,0 +1,44 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.subsystems.DriveTrainSubsystem;
+
+/**
+ * An example command. You can replace me with your own command.
+ */
+public class LimeLightArcadeDriveCommand extends Command {
+  private DriveTrainSubsystem driveTrain;
+
+  public LimeLightArcadeDriveCommand() {
+    // Use requires() here to declare subsystem dependencies
+    requires(Robot.driveTrain);
+    driveTrain = Robot.driveTrain;
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    driveTrain.stop();
+  }
+
+  // Read Controller Input from two joysticks.
+  // Left joystick controls the left motors and the right joystick controls the
+  // right motors
+  // Temporary until we get the XboxController wrapper for joystick
+  @Override
+  protected void execute() {
+    Robot.driveTrain.ArcadeDrive(Robot.m_oi.getController0().getY(Hand.kLeft), Robot.m_oi.getController0().getX(Hand.kLeft), true);
+  }
+
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  @Override
+  protected void end() {
+    driveTrain.stop();
+  }
+}
---
+++ FRC-Robot-2019/src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class LimeLightSubsystem extends Subsystem {
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+
+    public NetworkTable geNetworkTable() {
+        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+        NetworkTableEntry tx = table.getEntry("tx");
+        NetworkTableEntry ty = table.getEntry("ty");
+        NetworkTableEntry ta = table.getEntry("ta");
+        NetworkTableEntry tv = table.getEntry("tv");
+
+        //read values periodically
+        double x = tx.getDouble(0.0);
+        double y = ty.getDouble(0.0);
+        double area = ta.getDouble(0.0);
+        double target = tv.getDouble(0.0);
+
+        return table;
+    }
+
+}
