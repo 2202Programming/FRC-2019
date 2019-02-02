824102002b8caba2b3d0117a68a0fd4ee4efb203
Mohnish Kalia <20kaliam@elmbrookstudents.org>
2019-02-02 13:55:08 -0600

    Documentation for a few commands

--- FRC-Robot-2019/src/main/java/frc/robot/commands/OpenCargoTrapCommand.java
+++ FRC-Robot-2019/src/main/java/frc/robot/commands/OpenCargoTrapCommand.java
@@ -2,12 +2,19 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.InstantCommand;
 import frc.robot.Robot;
-
+/**
+ * Opens the cargo trap on the robot chassis.
+ */
 public class OpenCargoTrapCommand extends InstantCommand {
+    /**
+     * Constructor for the open cargo trap command. Requires a cargoTrap subsystem.
+     */
     public OpenCargoTrapCommand() {
         requires(Robot.cargoTrap);
     }
-
+    /**
+     * Traps the cargo within the arms.
+     */
     @Override
     protected void execute() {
         Robot.cargoTrap.openTrapArms();
--- FRC-Robot-2019/src/main/java/frc/robot/commands/RetractCargoTrapCommand.java
+++ FRC-Robot-2019/src/main/java/frc/robot/commands/RetractCargoTrapCommand.java
@@ -2,12 +2,19 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.InstantCommand;
 import frc.robot.Robot;
-
+/**
+ * Retracts the cargo trap on the robot chassis.
+ */
 public class RetractCargoTrapCommand extends InstantCommand {
+    /**
+     * Constructor for the retract cargo trap command. Requires a cargoTrap subsystem.
+     */
     public RetractCargoTrapCommand() {
         requires(Robot.cargoTrap);
     }
-
+    /**
+     * Retracts the cargo trap.
+     */
     @Override
     protected void execute() {
         Robot.cargoTrap.retractTrap();
