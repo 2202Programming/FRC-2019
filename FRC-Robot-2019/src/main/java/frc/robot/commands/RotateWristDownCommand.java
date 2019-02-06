d3f08fd251a09e7318632721dbf602856abf9ebf
Xander Ge <xander.q.ge@gmail.com>
2019-02-05 19:11:25 -0600

    Added Manual Wrist Rotation

---
+++ FRC-Robot-2019/src/main/java/frc/robot/commands/RotateWristDownCommand.java
@@ -0,0 +1,41 @@
+package frc.robot.commands; 
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.input.XboxControllerButtonCode;
+
+public class RotateWristDownCommand extends Command{
+
+    public RotateWristDownCommand(){
+        requires(Robot.intake);
+    }
+
+    @Override
+    protected void initialize() {
+        Robot.intake.stop();
+    }
+
+
+  @Override
+  protected void execute() {
+      Robot.intake.runWristDown();
+
+  }
+
+  @Override
+  protected boolean isFinished() {
+    return Robot.m_oi.getController1().getRawButtonReleased(XboxControllerButtonCode.BACK.getCode());
+  }
+
+ 
+  @Override
+  protected void end() {
+      Robot.intake.stop();
+  }
+
+
+  @Override
+  protected void interrupted() {
+      return;
+  }
+}
+
