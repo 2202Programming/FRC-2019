ad2029c73f0cfa6c463d11a230e21e597811d977
Unknown <20hussan@elmbrookstudents.org>
2019-02-06 19:40:15 -0600

    Skeleton of serial port subsystem

---
+++ FRC-Robot-2019/src/main/java/frc/robot/subsystems/SerialPortSubsystem.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems;
+
+/**
+ * What this subsystem does
+ */
+public class SerialPortSubsystem extends Subsystem {
+
+//stuff it needs
+
+  public SerialPortSubsystem() {
+//initialize
+  }
+
+  @Override
+  public void initDefaultCommand() {
+      //set default command
+      setDefaultCommand();
+  }
+}
