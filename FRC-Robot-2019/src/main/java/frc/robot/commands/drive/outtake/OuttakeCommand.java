c9451c4e81275b38893d7de860c8b49f0d5c68d2
Unknown <billhuang0916@gmail.com>
2019-02-11 19:04:13 -0600

    Simplified Outtake Command

--- FRC-Robot-2019/src/main/java/frc/robot/commands/drive/outtake/OuttakeCommand.java
+++ FRC-Robot-2019/src/main/java/frc/robot/commands/drive/outtake/OuttakeCommand.java
@@ -1,11 +1,11 @@
 package frc.robot.commands.drive.outtake;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.InstantCommand;
 import frc.robot.Robot;
 //import frc.robot.input.XboxControllerButtonCode;
 import frc.robot.subsystems.IntakeSubsystem;
 
-public class OuttakeCommand extends Command {
+public class OuttakeCommand extends InstantCommand {
     private IntakeSubsystem intake;
 
     public OuttakeCommand() {
@@ -15,32 +15,11 @@ public class OuttakeCommand extends Command {
 
     @Override
     protected void initialize() {
-    }
-
-    @Override
-    protected void execute() {
-        intake.vacuumOff();
-    }
-
-    @Override
-    protected void end() {
-        intake.vacuumOff(); // shouldn't be needed.
-    }
-
-    @Override
-    protected boolean isFinished() {
-        /**
-         * We once the motor is turned off, this command is finished, you shouldn't have
-         * to wait for a button release
-         * Robot.m_oi.getAssistentController().getRawButtonReleased(XboxControllerButtonCode.RB.getCode());
-         *
-         */
-        return true;
+        intake.vacuumOff();    
     }
 
     @Override
     protected void interrupted() {
-        return;
+        intake.vacuumOff();
     }
-
 }
