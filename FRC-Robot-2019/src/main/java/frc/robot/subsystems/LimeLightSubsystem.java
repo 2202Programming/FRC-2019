524aab68bd979c12e4804afab6a39072eb040294
Unknown <jruben4@yahoo.com>
2019-02-13 19:49:47 -0600

    Add cameraselect output

--- FRC-Robot-2019/src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
+++ FRC-Robot-2019/src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
@@ -4,6 +4,7 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.Robot;
 
 public class LimeLightSubsystem extends Subsystem {
 
@@ -24,12 +25,17 @@ public class LimeLightSubsystem extends Subsystem {
         NetworkTableEntry ty = table.getEntry("ty");
         NetworkTableEntry ta = table.getEntry("ta");
         NetworkTableEntry tv = table.getEntry("tv");
+        NetworkTableEntry cameraSelect = table.getEntry("cameraSelect");
 
         //read values periodically
         x = tx.getDouble(0.0);
         y = ty.getDouble(0.0);
         area = ta.getDouble(0.0);
         target = tv.getDouble(0.0);
+
+        //put cameraselect value into network tables
+        if(Robot.driveTrain.getInversionConstant()>0) cameraSelect.setBoolean(true);
+        else cameraSelect.setBoolean(false);
 
         return;
     }
