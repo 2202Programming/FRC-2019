6e7788201089de4bae70c87f4aa4e887dcb6159c
Unknown <jruben4@yahoo.com>
2019-02-26 21:18:47 -0600

    Switched to arrays

--- FRC-Robot-2019/src/main/java/frc/robot/subsystems/SerialPortSubsystem.java
+++ FRC-Robot-2019/src/main/java/frc/robot/subsystems/SerialPortSubsystem.java
@@ -8,26 +8,19 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.RobotMap;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.List;
+import java.util.ArrayList;
 
 //Listens on USB Serial port for LIDAR distance data from the arduino
 
 public class SerialPortSubsystem extends Subsystem {
 
-private int distance1; //Sensor #1
-private int distance2; //Sensor #2
-private int distance3; //Sensor #3
-private int distance4; //Sensor #4
-
-
+private int[] distanceArray = new int[4];
+private ArrayList<Deque> distanceAvgArray;
 private StringBuilder serialResults =new StringBuilder();
 private SerialPort arduinoSerial;
 private boolean serialExists = true;
 private long logTimer;
-
-private Deque<Integer> distance1Array = new ArrayDeque<Integer>();
-private Deque<Integer> distance2Array = new ArrayDeque<Integer>();
-private Deque<Integer> distance3Array = new ArrayDeque<Integer>();
-private Deque<Integer> distance4Array = new ArrayDeque<Integer>();
 
   public SerialPortSubsystem() {
     try {
@@ -38,6 +31,12 @@ private Deque<Integer> distance4Array = new ArrayDeque<Integer>();
     }
 
     logTimer = System.currentTimeMillis();
+    distanceAvgArray = new ArrayList<Deque>();
+    distanceAvgArray.add(0, new ArrayDeque<Integer>());
+    distanceAvgArray.add(1, new ArrayDeque<Integer>());
+    distanceAvgArray.add(2, new ArrayDeque<Integer>());
+    distanceAvgArray.add(3, new ArrayDeque<Integer>());
+
 
   }
 
@@ -50,81 +49,33 @@ private Deque<Integer> distance4Array = new ArrayDeque<Integer>();
       //set default command
   }
 
-  public int getDistance(int sensor) {
-    if (sensor==1) return distance1;
-    if (sensor==2) return distance2;
-    if (sensor==3) return distance3;
-    if (sensor==4) return distance4;
-    return 0;
+  public int getDistance(int sensor) { //return last distance measure for LIDAR sensor
+    return distanceArray[sensor];
   }
 
-  public Integer getDistanceAvg(int sensor) {
-    Integer[] tempArray;
+  public Integer getDistanceAvg(int sensor) { //return olympic average of last 10 readings of LIDAR sensor
+    
+    Deque<Integer> tempDeque = distanceAvgArray.get(sensor);
+    Integer[] tempArray = tempDeque.toArray(new Integer[0]); //switch from ArrayDeque to actual array so we can get each element
     Integer average = 0;
     Integer sum = 0;
     Integer max = 0;
     Integer min = 10000;
-    switch(sensor) { //choses array based on sensor then finds 'Olympic Rolling Avg' of past 10 sensor inputs
-    
-      case 1: tempArray = distance1Array.toArray(new Integer[0]);
-              for (int i = 0; i<distance1Array.size(); i++) {
-                if(tempArray[i] > max)
-                {
-                  max = tempArray[i];
-                }
-                if(tempArray[i] < min)
-                {
-                  min = tempArray[i];
-                }
-                sum = sum + tempArray[i];
-              }
-              average = (sum-max-min) / (distance1Array.size()-2);
-              break;
-      case 2: tempArray = distance2Array.toArray(new Integer[0]);
-              for (int i = 0; i<distance2Array.size(); i++) {
-                if(tempArray[i] > max)
-                {
-                  max = tempArray[i];
-                }
-                if(tempArray[i] < min)
-                {
-                  min = tempArray[i];
-                }
-                sum = sum + tempArray[i];
-              }
-              average = (sum-max-min) / (distance2Array.size()-2);
-              break;
-      case 3: tempArray = distance3Array.toArray(new Integer[0]);
-              for (int i = 0; i<distance3Array.size(); i++) {
-                if(tempArray[i] > max)
-                {
-                  max = tempArray[i];
-                }
-                if(tempArray[i] < min)
-                {
-                  min = tempArray[i];
-                }
-                sum = sum + tempArray[i];
-              }
-              average = (sum-max-min) / (distance3Array.size()-2);
-              break;
-      case 4: tempArray = distance4Array.toArray(new Integer[0]);
-              for (int i = 0; i<distance4Array.size(); i++) {
-                if(tempArray[i] > max)
-                {
-                  max = tempArray[i];
-                }
-                if(tempArray[i] < min)
-                {
-                  min = tempArray[i];
-                }
-                sum = sum + tempArray[i];
-              }
-              average = (sum-max-min) / (distance4Array.size()-2);
-              break;
-      }
 
-      return average; 
+    for (int i = 0; i<distanceAvgArray.get(sensor).size(); i++) {
+      if(tempArray[i] > max)
+      {
+        max = tempArray[i];
+      }
+      if(tempArray[i] < min)
+      {
+        min = tempArray[i];
+      }
+      sum = sum + tempArray[i];
+    }
+    average = (sum-max-min) / (distanceAvgArray.get(sensor).size()-2); //return average, not including max or min reading (olympic)  
+
+    return average; 
     }
 
 
@@ -248,31 +199,11 @@ private Deque<Integer> distance4Array = new ArrayDeque<Integer>();
 
     distance = Integer.parseInt(serialResults.substring(2)); //read in everything after first two char as a distance (in mm)
 
-    switch(sensor) { //set read distance to correct LIDAR based on read sensor ID
-      case 1: distance1=distance;
-              distance1Array.add(distance1);
-              if (distance1Array.size()>10) { //keep most recent 10 valuse in array
-                distance1Array.remove();
-              }
-              break;
-      case 2: distance2=distance;
-              distance2Array.add(distance2);
-              if (distance2Array.size()>10) {//keep most recent 10 valuse in array
-                distance2Array.remove();
-              }
-              break;
-      case 3: distance3=distance;
-              distance3Array.add(distance3);
-              if (distance3Array.size()>10) {//keep most recent 10 valuse in array
-                distance3Array.remove();
-              }
-              break;
-      case 4: distance4=distance;
-              distance4Array.add(distance4);
-              if (distance4Array.size()>10) {//keep most recent 10 valuse in array
-                distance4Array.remove();
-              }
-              break;
+    distanceArray[sensor]=distance;//set read distance to correct LIDAR based on read sensor ID
+    distanceAvgArray.get(sensor).add(distance);//add to rolling average array
+    if(distanceAvgArray.get(sensor).size() > 10)//keep most recent 10 valuse in array
+    {
+      distanceAvgArray.get(sensor).remove();
     }
   }
 }
