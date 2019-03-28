1acd824b39303bd00e55d139472eeda9ae676553
Unknown <billhuang0916@gmail.com>
2019-03-28 14:58:10 -0500

    Adjusted Safety Extensions

--- FRC-Robot-2019/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ FRC-Robot-2019/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -218,7 +218,7 @@ public class ArmSubsystem extends ExtendedSubSystem {
    * @return
    */
   public double getMinExtension(double angle) {
-    if(-35.0 < angle && angle < 35.0) {
+    if(-35.0 < angle && angle < 33.0) {
       return STARTING_EXTENSION;
     }
     return EXTEND_MIN;
