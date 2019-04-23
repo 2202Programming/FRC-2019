d481b78f6ef84b40d65d93923b0a9c578478edf9
Beast <BeastDriverStn2019@gmail.com>
2019-04-22 20:35:55 -0500

    Climb Works Again

--- FRC-Robot-2019/src/main/java/frc/robot/commands/climb/ClimbGroup.java
+++ FRC-Robot-2019/src/main/java/frc/robot/commands/climb/ClimbGroup.java
@@ -8,6 +8,7 @@ import frc.robot.commands.arm.MoveArmToPosition;
 import frc.robot.commands.arm.MoveArmToRawPosition;
 import frc.robot.commands.drive.DriveByPowerAndJoystickCommand;
 import frc.robot.commands.drive.HABDriveByPowerAndJoystickCommand;
+import frc.robot.commands.intake.VacuumCommand;
 import frc.robot.commands.intake.WristSetAngleCommand;
 import frc.robot.commands.intake.WristTrackAngle;
 import frc.robot.commands.util.Angle;
@@ -18,10 +19,11 @@ public class ClimbGroup extends CommandGroup {
         double rollPower = 0.6;
         double drivePower = 0.4; // Positive power goes to negative direction
 
-        //if separate command to bring up robot change to parallel
+        // Climb Sequence
         addSequential(Robot.climber.zeroSubsystem());   //hack to zero counters
-        addSequential(new WristSetAngleCommand(0));
-        addSequential(new MoveArmToRawPosition(-35, 1, 0.5, 180));    
+        addSequential(new VacuumCommand(false, 0.02));
+        addSequential(new WristSetAngleCommand(-90));
+        addSequential(new MoveArmToRawPosition(-90, 12, 0.5, 180));    
         addSequential(new PawlSureFire(Robot.climber.Extend, 4));
         addSequential(new DeployClimbFoot(1.0, climbHeight));    // 20.5 uses limit switch
         addSequential(new WaitCommand(0.5));
@@ -30,15 +32,26 @@ public class ClimbGroup extends CommandGroup {
         forwardCmds.addParallel(new ClimbRollForward(0.15, rollPower, 0.2));   // power, timeout
         forwardCmds.addParallel(new HABDriveByPowerAndJoystickCommand(drivePower, 0.25, 0.6)); // power, timeout
         
+
+        // CommandGroup skidGroup = new CommandGroup("Going foward with skid");
+        // skidGroup.addSequential(new WaitCommand(1.5));
+        // skidGroup.addSequential(new MoveArmToRawPosition(-133, 19.0, 0.5, 180));
+       
+        
+        // Drive into HAB Sequence
+        addSequential(new MoveArmToRawPosition(-133, 19.0, 0.5, 180));        
         addSequential(forwardCmds);
         CommandGroup forwardCmds3 = new CommandGroup("Going forward 2");
         forwardCmds3.addSequential(new PawlSureFire(Robot.climber.Retract,  6));
         forwardCmds3.addParallel(new DeployClimbFoot(-1.0, retractHeight));    // neg power retract / limit sw
         forwardCmds3.addParallel(new DriveByPowerAndJoystickCommand(drivePower, 0.25, 0.6, timeToDriveForward)); // neg power drive reverse
         
-        addSequential(new MoveArmToRawPosition(-90, 12, 0.6, 180));
+        // Retract Sequence
+        addSequential(new WristSetAngleCommand(0));
+        addSequential(new MoveArmToRawPosition(-90, 18, 0.6, 180));
         addSequential(forwardCmds3);
 
+        //Drive fully onto HAB sequence 
         addSequential(new MoveArmToRawPosition(-90, 6, 0.6, 180));
         addParallel(new WristTrackAngle(Angle.Back_Perpendicular_Down.getAngle()));
         addSequential(new DriveByPowerAndJoystickCommand(drivePower, 0.25, 0.6, 200.0));
